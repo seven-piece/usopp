dfa733334b277529ed73606687372c29f5deaf48
Kallol Bairagi <54534189+kbnewbee@users.noreply.github.com>
2023-10-19 12:06:03 +0530

    basic draft changes
    basic draft changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,23 +87,23 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
 });
 
 /*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper('.testimonial__container', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 48,
+// let swiperTestimonial = new Swiper('.testimonial__container', {
+//     loop: true,
+//     grabCursor: true,
+//     spaceBetween: 48,
 
 
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    },
-    breakpoints:{
-        568:{
-            slidesPerview: 2,
-        }
-    }
-});
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//         dynamicBullets: true,
+//     },
+//     breakpoints:{
+//         568:{
+//             slidesPerview: 2,
+//         }
+//     }
+// });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
